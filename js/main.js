40e9f1559480a9ee727b4af21f2d29a908b03a93
William Hall <billhall168@gmail.com>
2020-07-14 16:27:16 -0700

    added jquery counter and vote button

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("Does this work?");
+
+// I want to select the vote button, and count the votes, and display them on the top of the page.
+$(document).ready(function() {
+
+    let count = 0;
+    $(".vote").on("click", function() {
+        count ++;
+      $("#votes").text(`Unofficially, Caryn has ${count} votes!`)
+    });
+
+  });
